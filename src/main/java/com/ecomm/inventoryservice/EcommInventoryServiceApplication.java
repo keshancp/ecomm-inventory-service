319ae01e63398655a543985c5277f95c3eb3de93
keshancp <keshanc.perera@gmail.com>
2022-10-24 00:43:27 +0530

    initial commit

---
+++ src/main/java/com/ecomm/inventoryservice/EcommInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomm/inventoryservice/EcommInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
